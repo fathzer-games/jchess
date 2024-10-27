ef3893b8a849059bb34e877ecb9961e124eb4bbc
Astesana <jm-dev@astesana.net>
2024-10-27 17:42:42 +0100

    Start working on headless tournaments

---
+++ src/main/java/com/fathzer/jchess/AbstractGameSession.java
@@ -0,0 +1,251 @@
+package com.fathzer.jchess;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+import com.fathzer.games.Color;
+import com.fathzer.games.Status;
+import com.fathzer.jchess.bot.Engine;
+import com.fathzer.jchess.bot.uci.EngineLoader;
+import com.fathzer.jchess.bot.uci.EngineLoader.EngineData;
+import com.fathzer.games.clock.Clock;
+import com.fathzer.games.clock.ClockSettings;
+import com.fathzer.jchess.settings.Settings;
+import com.fathzer.jchess.settings.Settings.ColorSetting;
+import com.fathzer.jchess.settings.Settings.EngineSettings;
+import com.fathzer.util.Observable;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractGameSession<T> {
+	public enum State {
+		CREATED, PAUSED, RUNNING, ENDED
+	}
+	
+	protected final T gui;
+	private Settings settings;
+	private Engine whiteEngine;
+	private Engine blackEngine;
+	private Score score;
+	protected Color player1Color;
+	private Observable<State> state;
+	protected Game game;
+
+	public Score getScore() {
+		return score;
+	}
+	
+	protected AbstractGameSession(T gui, Settings settings) {
+		this.gui = gui;
+		this.state = new Observable<>(State.CREATED);
+		state.addListener(this::onStateChanged);
+		score = new Score();
+		setSettings(settings);
+	}
+	
+	protected void onStateChanged(State old, State current) {
+		log.debug("State changed from {} to {}", old,current);
+		if (State.RUNNING.equals(current)) {
+			game.start();
+		} else {
+			game.pause();
+		}
+	}
+
+	private void doRevenge() {
+		final Color previous1 = player1Color;
+		if (ColorSetting.RANDOM.equals(settings.getPlayer1Color()) && score.getGameCount()%2==0) {
+			player1Color = settings.getPlayer1Color().getColor();
+		} else {
+			player1Color = player1Color.opposite();
+		}
+		if (!previous1.equals(player1Color)) {
+			// Switch engines color
+			Engine dummy = whiteEngine;
+			whiteEngine = blackEngine;
+			blackEngine = dummy;
+			onPlayerColorsChanged();
+		}
+		initGame();
+		setState(State.PAUSED);
+		start();
+	}
+	
+	protected void onPlayerColorsChanged() {
+		// Allows subclasses to perform extra initialization when players color changes
+	}
+	
+	protected void initGame() {
+		this.game = new Game(settings.getVariant().getRules().apply(settings.getFen()), buildClock());
+		this.game.setStartClockAfterFirstMove(settings.isStartClockAfterFirstMove());
+		score.newGame();
+	}
+
+	private Clock buildClock() {
+		if (settings.getClock()!=null) {
+			final ClockSettings common = settings.getClock().toClockSettings();
+			final int extraTime = Integer.getInteger("player2ExtraTimeS",0);
+			final Clock clock;
+			if (extraTime==0) {
+				clock = new Clock(common);
+			} else {
+				final ClockSettings other = new ClockSettings(common.getInitialTime()+extraTime).withIncrement(common.getIncrement(), common.getMovesNumberBeforeIncrement(), common.isCanAccumulate());
+				clock = player1Color==Color.WHITE ? new Clock(common, other) : new Clock(other, common);
+			}
+			clock.addStatusListener(this::timeUp);
+			clock.addClockListener(e -> log.debug("Clock {} state changes from {} to {}",e.getClock(), e.getPreviousState(), e.getNewState()));
+			if (settings.isStartClockAfterFirstMove()) {
+				clock.withStartingColor(Color.BLACK);
+			}
+			return clock;
+		} else {
+			return null;
+		}
+	}
+	
+	public Engine getEngine(EngineSettings settings) {
+		if (settings==null) {
+			return null;
+		}
+		final Optional<EngineData> found = EngineLoader.getEngines().stream().filter(e -> e.getEngine()!=null && e.getName().equals(settings.getName())).findAny();
+		return found.orElseThrow().getEngine();
+	}
+
+	protected boolean onlyHumans() {
+		return this.settings.getPlayer1().getEngine()==null && this.settings.getPlayer2().getEngine()==null;
+	}
+	
+	public void addListener(BiConsumer<State,State> listener) {
+		this.state.addListener(listener);
+	}
+	
+	public void setEngine(Color color, Engine engine) {
+		if (Color.WHITE.equals(color)) {
+			this.whiteEngine = engine;
+		} else if (Color.BLACK.equals(color)) {
+			this.blackEngine = engine;
+		}
+	}
+	
+	protected Engine getEngine(Color color) {
+		if (Color.WHITE.equals(color)) {
+			return this.whiteEngine;
+		} else if (Color.BLACK.equals(color)) {
+			return this.blackEngine;
+		} else {
+			throw new NullPointerException();
+		}
+	}
+	
+	public void start() {
+		if (State.ENDED.equals(getState())) {
+			this.score.reset();
+			initGame();
+		}
+		setEngine(player1Color, getEngine(settings.getPlayer1().getEngine()));
+		setEngine(player1Color.opposite(), getEngine(settings.getPlayer2().getEngine()));
+		setState(State.RUNNING);
+	}
+
+	protected abstract void nextMove();
+	
+	protected abstract void play(Game game, Move move);
+
+	public State getState() {
+		return this.state.getValue();
+	}
+	
+	private void setState(State state) {
+		this.state.setValue(state);
+	}
+
+	protected abstract void onMove(Move move);
+	
+	/** This method is called when clock emits a time up event.
+	 * <br>Please note that this method could be invoked on a thread that is not the Swing event thread. 
+	 * @param status The game status.
+	 */
+	protected void timeUp(Status status) {
+	}
+	
+	protected void doTimeUp(Status status) {
+		if (getTournamentGamesCount()==0) {
+			// If we are not in tournament mode, propose to continue without clock
+			this.setState(State.PAUSED);
+			if (continueOnTimeup(status)) {
+				this.setState(State.RUNNING);
+				return;
+			}
+		}
+		endOfGame(status);
+	}
+	
+	protected boolean continueOnTimeup(Status status) {
+		return false;
+	}
+	
+	protected void resign(Color color) {
+		if (!getState().equals(State.RUNNING)) {
+			return;
+		}
+		setState(State.PAUSED);
+		if (game.getBoard().getActiveColor().equals(color) && isResignationConfirmed()) {
+			final Status status = Color.WHITE.equals(color) ? Status.BLACK_WON : Status.WHITE_WON;
+			endOfGame(status);
+		} else {
+			setState(State.RUNNING);
+		}
+	}
+	
+	protected boolean isResignationConfirmed() {
+		return true;
+	}
+	
+	public int getTournamentGamesCount() {
+		return Integer.getInteger("gameCount",0);
+	}
+
+	protected void endOfGame(final Status status) {
+		setState(State.PAUSED);
+		log.debug("End of game,  state: {}", getState());
+		try {
+			GameRecorder.commit(this.settings, this.player1Color, this.game.getHistory());
+		} catch (Exception e) {
+			log.error("An error occured while writing pgn",e);
+		}
+		updateScores(status);
+		if (isMakeRevenge(status)) {
+			doRevenge();
+		} else {
+			setState(State.ENDED);
+		}
+	}
+	
+	protected boolean isMakeRevenge(final Status status) {
+		final int toPlay = getTournamentGamesCount();
+		return toPlay!=0 && score.getGameCount()<toPlay;
+	}
+	
+	protected void updateScores(Status status) {
+		if (Status.DRAW.equals(status)) {
+			score.draw();
+		} else {
+			score.win(status.winner()==player1Color);
+		}
+	}
+	
+	public void stop() {
+		setState(State.PAUSED);
+	}
+
+	public void setSettings(Settings settings) {
+		if (State.RUNNING.equals(getState()) || State.PAUSED.equals(getState())) {
+			throw new IllegalStateException("Can't change the game settings during the game");
+		}
+		this.settings = settings;
+		player1Color = settings.getPlayer1Color().getColor();
+		score.reset();
+		initGame();
+	}
+}
--- src/main/java/com/fathzer/jchess/swing/GameSession.java
+++ src/main/java/com/fathzer/jchess/swing/GameSession.java
@@ -1,63 +1,32 @@
 package com.fathzer.jchess.swing;
 
-import java.util.Optional;
-import java.util.function.BiConsumer;
-
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import com.fathzer.games.Color;
 import com.fathzer.games.Status;
+import com.fathzer.jchess.AbstractGameSession;
 import com.fathzer.jchess.Game;
-import com.fathzer.jchess.GameRecorder;
 import com.fathzer.jchess.Move;
-import com.fathzer.jchess.Score;
 import com.fathzer.jchess.ai.evaluator.NaiveEvaluator;
 import com.fathzer.jchess.bot.Engine;
-import com.fathzer.jchess.bot.uci.EngineLoader;
-import com.fathzer.jchess.bot.uci.EngineLoader.EngineData;
-import com.fathzer.games.clock.Clock;
-import com.fathzer.games.clock.ClockSettings;
 import com.fathzer.jchess.settings.Settings;
-import com.fathzer.jchess.settings.Settings.ColorSetting;
-import com.fathzer.jchess.settings.Settings.EngineSettings;
-import com.fathzer.util.Observable;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class GameSession {
-	public enum State {
-		CREATED, PAUSED, RUNNING, ENDED
-	}
-	
-	private final GamePanel panel;
-	private Settings settings;
-	private Engine whiteEngine;
-	private Engine blackEngine;
-	private Score score;
-	private Color player1Color;
-	private Observable<State> state;
-	private Game game;
+public class GameSession extends AbstractGameSession<GamePanel> {
 	private long lastMoveTime = 0;
 
 	public GameSession(GamePanel panel, Settings settings) {
-		this.panel = panel;
-		this.state = new Observable<>(State.CREATED);
-		state.addListener(this::onStateChanged);
+		super(panel, settings);
 		panel.getBoard().addPropertyChangeListener(ChessBoardPanel.TARGET, evt -> onMove((Move) evt.getNewValue()));
 		panel.setResignationHandler(this::resign);
-		score = new Score();
-		setSettings(settings);
 	}
 	
-	private void onStateChanged(State old, State current) {
-		log.debug("State changed from {} to {}", old,current);
-		if (State.RUNNING.equals(current)) {
-			game.start();
-		} else {
-			game.pause();
-		}
+	@Override
+	protected void onStateChanged(State old, State current) {
+		super.onStateChanged(old,  current);
 		if (SwingUtilities.isEventDispatchThread()) {
 			swingStateChanged(current);
 		} else {
@@ -69,116 +38,36 @@ public class GameSession {
 		if (State.RUNNING.equals(current)) {
 			nextMove();
 		} else {
-			panel.getBoard().setManualMoveEnabled(false);
+			gui.getBoard().setManualMoveEnabled(false);
+		}
+	}
+
+	@Override
+	protected void onPlayerColorsChanged() {
+		gui.getBoard().setReverted(Color.BLACK.equals(player1Color));
+		if (onlyHumans()) {
+			// Change upside down color
+			gui.getBoard().setUpsideDownColor(player1Color.opposite());
 		}
 	}
 	
-	private void doRevenge() {
-		final Color previous1 = player1Color;
-		if (ColorSetting.RANDOM.equals(settings.getPlayer1Color()) && score.getGameCount()%2==0) {
-			player1Color = settings.getPlayer1Color().getColor();
-		} else {
-			player1Color = player1Color.opposite();
-		}
-		panel.getBoard().setReverted(Color.BLACK.equals(player1Color));
-		if (!previous1.equals(player1Color)) {
-			// Switch engines color
-			Engine dummy = whiteEngine;
-			whiteEngine = blackEngine;
-			blackEngine = dummy;
-			if (onlyHumans()) {
-				// Change upside down color
-				panel.getBoard().setUpsideDownColor(player1Color.opposite());
-			}
-		}
-		initGame();
-		setState(State.PAUSED);
-		start();
-	}
-	
-	private void initGame() {
-		this.game = new Game(settings.getVariant().getRules().apply(settings.getFen()), buildClock());
-		this.game.setStartClockAfterFirstMove(settings.isStartClockAfterFirstMove());
-		panel.setPlayer1Color(player1Color);
-		panel.setClock(game.getClock());
-		panel.getBoard().setBoard(game.getBoard());
-		panel.getBoard().setManualMoveEnabled(false);
-		score.newGame();
+	@Override
+	protected void initGame() {
+		super.initGame();
 		setEvaluation();
+		gui.setPlayer1Color(player1Color);
+		gui.setClock(game.getClock());
+		gui.setScore(getScore());
+		gui.getBoard().setBoard(game.getBoard());
+		gui.getBoard().setManualMoveEnabled(false);
 		lastMoveTime = System.currentTimeMillis();
 	}
-
-	private Clock buildClock() {
-		if (settings.getClock()!=null) {
-			final ClockSettings common = settings.getClock().toClockSettings();
-			final int extraTime = Integer.getInteger("player2ExtraTimeS",0);
-			final Clock clock;
-			if (extraTime==0) {
-				clock = new Clock(common);
-			} else {
-				final ClockSettings other = new ClockSettings(common.getInitialTime()+extraTime).withIncrement(common.getIncrement(), common.getMovesNumberBeforeIncrement(), common.isCanAccumulate());
-				clock = player1Color==Color.WHITE ? new Clock(common, other) : new Clock(other, common);
-			}
-			clock.addStatusListener(this::timeUp);
-			clock.addClockListener(e -> log.debug("Clock {} state changes from {} to {}",e.getClock(), e.getPreviousState(), e.getNewState()));
-			if (settings.isStartClockAfterFirstMove()) {
-				clock.withStartingColor(Color.BLACK);
-			}
-			return clock;
-		} else {
-			return null;
-		}
-	}
 	
-	public Engine getEngine(EngineSettings settings) {
-		if (settings==null) {
-			return null;
-		}
-		final Optional<EngineData> found = EngineLoader.getEngines().stream().filter(e -> e.getEngine()!=null && e.getName().equals(settings.getName())).findAny();
-		return found.orElseThrow().getEngine();
-	}
-
-	private boolean onlyHumans() {
-		return this.settings.getPlayer1().getEngine()==null && this.settings.getPlayer2().getEngine()==null;
-	}
-	
-	public void addListener(BiConsumer<State,State> listener) {
-		this.state.addListener(listener);
-	}
-	
-	public void setEngine(Color color, Engine engine) {
-		if (Color.WHITE.equals(color)) {
-			this.whiteEngine = engine;
-		} else if (Color.BLACK.equals(color)) {
-			this.blackEngine = engine;
-		}
-	}
-	
-	private Engine getEngine(Color color) {
-		if (Color.WHITE.equals(color)) {
-			return this.whiteEngine;
-		} else if (Color.BLACK.equals(color)) {
-			return this.blackEngine;
-		} else {
-			throw new NullPointerException();
-		}
-	}
-	
-	public void start() {
-		if (State.ENDED.equals(getState())) {
-			this.score.reset();
-			initGame();
-			panel.setScore(this.score);
-		}
-		setEngine(player1Color, getEngine(settings.getPlayer1().getEngine()));
-		setEngine(player1Color.opposite(), getEngine(settings.getPlayer2().getEngine()));
-		setState(State.RUNNING);
-	}
-
-	private void nextMove() {
+	@Override
+	protected void nextMove() {
 		final Color activeColor = game.getBoard().getActiveColor();
 		final Engine engine = getEngine(activeColor);
-		panel.getBoard().setManualMoveEnabled(engine==null);
+		gui.getBoard().setManualMoveEnabled(engine==null);
 		if (engine!=null) {
 			log.debug("Engine detected for {}",activeColor);
 			game.playEngine(engine, this::play, e -> {
@@ -188,33 +77,27 @@ public class GameSession {
 		}
 	}
 	
-	private void play(Game game, Move move) {
+	@Override
+	protected void play(Game game, Move move) {
 		SwingUtilities.invokeLater(() -> {
 			// WARNING: If a revenge is launched before the engine returns its choice, state can be RUNNING again
 			// and the move would be transmitted to the panel if we omitted to check we are still in the same game!
 			if (move!=null && game==this.game && State.RUNNING.equals(getState())) {
 				log.debug("Transmitting {}'s move {} to panel's board",game.getBoard().getActiveColor(),move);
-				panel.getBoard().doMove(move);
+				gui.getBoard().doMove(move);
 			} else {
 				log.debug("Ignore move {}, state is {}", move, getState());
 			}
 		});
 	}
 
-	public State getState() {
-		return this.state.getValue();
-	}
-	
-	private void setState(State state) {
-		this.state.setValue(state);
-	}
-
-	private void onMove(Move move) {
-		panel.repaint();
+	@Override
+	protected void onMove(Move move) {
+		gui.repaint();
 		this.game.onMove(move);
 		setEvaluation();
 //TODO		think(5000);
-		final Status status = panel.getBoard().getStatus();
+		final Status status = gui.getBoard().getStatus();
 		if (!Status.PLAYING.equals(status)) {
 			// Game is ended
 			endOfGame(status);
@@ -223,12 +106,6 @@ public class GameSession {
 				nextMove();
 			}
 		}
-	}
-	
-	private void setEvaluation() {
-		final NaiveEvaluator ev = new NaiveEvaluator();
-		ev.init(game.getBoard());
-		panel.setEvaluation(ev.evaluateAsWhite(game.getBoard())/100);
 	}
 	
 	// This makes, always when two bots are competing, and sometime when human plays against a bot,
@@ -256,82 +133,48 @@ public class GameSession {
 		}
 	}
 
+	private void setEvaluation() {
+		final NaiveEvaluator ev = new NaiveEvaluator();
+		ev.init(game.getBoard());
+		gui.setEvaluation(ev.evaluateAsWhite(game.getBoard())/100);
+	}
 
-	/** This method is called when clock emits a time up event.
-	 * <br>Please note that this method could be invoked on a thread that is not the Swing event thread. 
-	 * @param status The game status.
-	 */
-	private void timeUp(Status status) {
+	@Override
+	protected void timeUp(Status status) {
 		SwingUtilities.invokeLater(()->doTimeUp(status));
 	}
 	
-	private void doTimeUp(Status status) {
-		if (getTournamentGamesCount()==0) {
-			// If we are not in tournament mode, propose to continue without clock
-			this.setState(State.PAUSED);
-			if (JOptionPane.showConfirmDialog(panel, getMessage(status)+" Do you want to continue game without clock?","Time is up",JOptionPane.YES_NO_OPTION)==0) {
-				this.setState(State.RUNNING);
-				return;
-			}
-		}
-		endOfGame(status);
+	@Override
+	protected boolean continueOnTimeup(Status status) {
+		return JOptionPane.showConfirmDialog(gui, getMessage(status)+" Do you want to continue game without clock?","Time is up",JOptionPane.YES_NO_OPTION)==0;
 	}
-	
-	private void resign(Color color) {
-		if (!getState().equals(State.RUNNING)) {
-			return;
-		}
-		setState(State.PAUSED);
-		if (game.getBoard().getActiveColor().equals(color) && JOptionPane.showConfirmDialog(panel, "Are you sure you want to resign?","Resignation",JOptionPane.YES_NO_OPTION)==0) {
-			final Status status = Color.WHITE.equals(color) ? Status.BLACK_WON : Status.WHITE_WON;
-			endOfGame(status);
-		} else {
-			setState(State.RUNNING);
-		}
-	}
-	
-	private int getTournamentGamesCount() {
-		return Integer.getInteger("gameCount",0);
+
+	@Override
+	protected boolean isResignationConfirmed() {
+		return JOptionPane.showConfirmDialog(gui, "Are you sure you want to resign?","Resignation",JOptionPane.YES_NO_OPTION)==0;
 	}
 	
 	private void onEngineError(Engine engine) {
-		JOptionPane.showMessageDialog(panel, "An error occured while communicating with the "+engine.getName()+" engine. Assuming it resigns", "Error", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(gui, "An error occured while communicating with the "+engine.getName()+" engine. Assuming it resigns", "Error", JOptionPane.ERROR_MESSAGE);
 		final Status status = Color.WHITE.equals(game.getBoard().getActiveColor()) ? Status.BLACK_WON : Status.WHITE_WON;
 		endOfGame(status);
 	}
-
-	private void endOfGame(final Status status) {
-		setState(State.PAUSED);
-		log.debug("End of game,  state: {}", getState());
-		try {
-			GameRecorder.commit(this.settings, this.player1Color, this.game.getHistory());
-		} catch (Exception e) {
-			log.error("An error occured while writing pgn",e);
-		}
-		updateScores(status);
-		final int toPlay = getTournamentGamesCount();
-		boolean makeRevenge;
-		if (toPlay==0) {
-			final String revenge = "Revenge";
-			int choice = JOptionPane.showOptionDialog(panel, getMessage(status), "End of game", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[] {revenge,"Enough for today"}, revenge);
-			makeRevenge = choice==0;
-		} else {
-			makeRevenge = score.getGameCount()<toPlay;
-		}
-		if (makeRevenge) {
-			doRevenge();
-		} else {
-			setState(State.ENDED);
-		}
-	}
 	
-	private void updateScores(Status status) {
-		if (Status.DRAW.equals(status)) {
-			score.draw();
-		} else {
-			score.win(status.winner()==player1Color);
+	@Override
+	protected boolean isMakeRevenge(final Status status) {
+		boolean result = super.isMakeRevenge(status);
+		if (!result && getTournamentGamesCount()==0) {
+			final String revenge = "Revenge";
+			int choice = JOptionPane.showOptionDialog(gui, getMessage(status), "End of game", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[] {revenge,"Enough for today"}, revenge);
+			result = choice==0;
 		}
-		panel.setScore(score);
+		return result;
+	}
+
+	@Override
+	protected void updateScores(Status status) {
+		super.updateScores(status);
+		gui.setScore(getScore());
 	}
 	
 	private String getMessage(Status status) {
@@ -345,28 +188,23 @@ public class GameSession {
 			throw new IllegalArgumentException();
 		}
 	}
-	
-	public void stop() {
-		setState(State.PAUSED);
+
+	@Override
+	public void setSettings(Settings settings) {
+		super.setSettings(settings);
+		refreshAppearance(settings);
 	}
 
-	public void setSettings(Settings settings) {
-		if (State.RUNNING.equals(getState()) || State.PAUSED.equals(getState())) {
-			throw new IllegalStateException("Can't change the game settings during the game");
-		}
-		this.settings = settings;
-		panel.setPlayer1Human(settings.getPlayer1().getEngine()==null);
-		panel.setPlayer2Human(settings.getPlayer2().getEngine()==null);
-		panel.getBoard().setShowPossibleMoves(settings.isShowPossibleMoves());
-		panel.getBoard().setTouchMove(settings.isTouchMove());
-		player1Color = settings.getPlayer1Color().getColor();
-		panel.getBoard().setReverted(Color.BLACK.equals(player1Color));
+	private void refreshAppearance(Settings settings) {
+		gui.setPlayer1Human(settings.getPlayer1().getEngine()==null);
+		gui.setPlayer2Human(settings.getPlayer2().getEngine()==null);
+		gui.getBoard().setShowPossibleMoves(settings.isShowPossibleMoves());
+		gui.getBoard().setTouchMove(settings.isTouchMove());
+		gui.getBoard().setReverted(Color.BLACK.equals(player1Color));
 		if (onlyHumans() && settings.isTabletMode()) {
-			panel.getBoard().setUpsideDownColor(player1Color.opposite());
+			gui.getBoard().setUpsideDownColor(player1Color.opposite());
 		} else {
-			panel.getBoard().setUpsideDownColor(null);
+			gui.getBoard().setUpsideDownColor(null);
 		}
-		score.reset();
-		initGame();
 	}
 }
